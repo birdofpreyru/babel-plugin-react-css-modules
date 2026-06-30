650ba835305ee867e520617aac075bd41303e137
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2026-07-01 01:55:33 +0200

    v7.0.1: Hotfixes

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,7 @@
 module.exports = {
   plugins: [
     '@babel/plugin-transform-flow-strip-types',
+    '@dr.pogodin/add-import-extension',
     ['babel-plugin-transform-import-meta', { module: 'ES6' }],
   ],
   presets: ['@babel/env'],
